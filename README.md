fa377430e37fb7ff23f46be2c2deb21923164897
Oaklight <pding.dp@qq.com>
2018-04-28 19:32:49 +0800

    adding the setup scripts with dependency updating, repo cloning and software building

---
+++ README.md
@@ -0,0 +1,3 @@
+# Monero on VPS
+Mining Monero using clusters of cheap VPS is an experimental attempt. <br>
+Since all my VPS run on CentOS 6.8 32-bit, the scripts are only intended for 32-bit machines for now.
---
+++ prepare-to-mine.sh
@@ -0,0 +1,34 @@
+# install yum-utils for yum-config-manager
+yum install yum-utils
+
+# install centos devtoolset-7 and set active
+yum install  epel-release
+yum install git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
+
+# on 32-bit platform
+
+# add 3rd-party repo for i686 build scl
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/mlampe/devtoolset-7/repo/epel-6/mlampe-devtoolset-7-epel-6.repo
+scl enable devtoolset-7
+
+# on 64-bit platform
+#
+
+# install libuv dependency
+git clone https://github.com/libuv/libuv.git || true
+
+cd libuv
+sh autogen.sh
+./configure
+make
+make check
+make install
+
+# install xmrig
+git clone https://github.com/xmrig/xmrig.git || true
+
+cd xmrig
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
