51d02e46d2b612498cb3c72896004559814f8027
Oaklight <pding.dp@qq.com>
2018-05-08 00:35:22 +0800

    change git clone to wget

--- prepare-to-mine.sh
+++ prepare-to-mine.sh
@@ -1,12 +1,9 @@
 # install yum-utils for yum-config-manager
 yum install yum-utils
-
 # install centos devtoolset-7 and set active
 yum install  epel-release
 yum install git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
-
 # on 32-bit platform
-
 # add 3rd-party repo for i686 build scl
 yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/mlampe/devtoolset-7/repo/epel-6/mlampe-devtoolset-7-epel-6.repo
 yum install devtoolset-7-toolchain
@@ -14,32 +11,37 @@ scl enable devtoolset-7
 
 # on 64-bit platform
 #
-mkdir dependencies && cd $_
+
+mkdir - p dependencies && cd $_
 
 # install libuv dependency
 # dependency existance check first
 # ldconfig -p | grep libuv
-git clone https://github.com/libuv/libuv.git || true
-
-cd libuv
+wget https://github.com/libuv/libuv/archive/v1.x.zip
+unzip master.zip
+rm master.zip
+cd libuv-master
+# build and install libuv
 sh autogen.sh
 ./configure
 make
 # make check
 make install
 cd ..
+rm libuv-master -rf
 
 # install xmrig
-git clone https://github.com/xmrig/xmrig.git || true
-
-cd xmrig
-# reset the contribution to 0
+wget https://github.com/xmrig/xmrig/archive/master.zip
+unzip master.zip
+rm master.zip
+cd xmrig-master
+# reset the donate-level to 0
 sed -i 's/kDonateLevel = 5/kDonateLevel = 0/1' src/donate.h
-
+# build xmrig
 mkdir -p build && cd $_
 cmake -DCMAKE_BUILD_TYPE=Release -DWITH_HTTPD=OFF ../
 make
-
+# copy the build to outer folder
 mv xmrig ../../../xmrig
 cd ../../..
 
